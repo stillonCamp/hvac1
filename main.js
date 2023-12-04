204595d94b658ec5b119726ae95c6d144d15ffdf
stillonCamp <135005958+stillonCamp@users.noreply.github.com>
2023-12-03 22:06:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function redirectToIndex() {
+  window.location.href = 'index.html';
+}
+
+function redirectToReviews() {
+    window.location.href = 'reviews.html';
+  }
+
+function redirectToContacts() {
+    window.location.href = 'send.html';
+  }
